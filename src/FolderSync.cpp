274bbd43077e332a4b57b6d9b81dbb1c69b070cd
dansyl1 <67522741+dansyl1@users.noreply.github.com>
2024-08-09 12:18:09 -0400

    Rebased on Pull Request 132

--- src/FolderSync.cpp
+++ src/FolderSync.cpp
@@ -637,13 +637,6 @@ int CFolderSync::SyncFolder(const PairData& pt)
                     std::wstring cryptPath = CPathUtils::Append(pt.m_cryptPath, it->first);
                     std::wstring origPath  = CPathUtils::Append(pt.m_origPath, it->first);
                     CCircularLog::Instance()(_T("INFO:    copy file %s to %s"), origPath.c_str(), cryptPath.c_str());
-                    if (pt.m_syncDir == BothWays)
-                    {
-                        // file does not exist in the encrypted folder, we can ignore
-                        // its creation.
-                        CAutoWriteLock nLocker(m_notingGuard);
-                        m_notifyIgnores.insert(CPathUtils::Append(pt.m_cryptPath, it->first));
-                    }
                     bool bCopyFileResult = CopyFile(origPath.c_str(), cryptPath.c_str(), FALSE);
                     if (!bCopyFileResult)
                     {
@@ -664,13 +657,6 @@ int CFolderSync::SyncFolder(const PairData& pt)
                 {
                     std::wstring cryptPath = CPathUtils::Append(pt.m_cryptPath, GetEncryptedFilename(it->first, pt.m_password, pt.m_encNames, pt.m_encNamesNew, pt.m_use7Z, pt.m_useGpg));
                     std::wstring origPath  = CPathUtils::Append(pt.m_origPath, it->first);
-                    if (pt.m_syncDir == BothWays)
-                    {
-                        // file does not exist in the encrypted folder, we can ignore
-                        // its creation.
-                        CAutoWriteLock nLocker(m_notingGuard);
-                        m_notifyIgnores.insert(cryptPath);
-                    }
                     if (!EncryptFile(origPath, cryptPath, pt.m_password, it->second, pt.m_useGpg, bCryptOnly, pt.m_compressSize, pt.m_ResetOriginalArchAttr))
                         retVal |= ErrorCrypt;
                 }
